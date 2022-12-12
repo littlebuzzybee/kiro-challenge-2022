ecceffbf97c960cdb732e53c74dadb48be3520a4
Matthias Personnaz <matthias.personnaz@ecl20.ec-lyon.fr>
2022-12-12 16:20:52 +0100

    Continuation de mon script

--- tests_matthias.jl
+++ tests_matthias.jl
@@ -114,14 +114,7 @@ function solution_cost(nb_jobs::Int64,
     return S
 end
 
-using AutoHashEquals;
-@auto_hash_equals struct State
-    todo::Int64
-    score::Float16
-    adressable::Int64
-    Dm::Int64 # n'encode pas l'application machine   -> tâche
-    Do::Int64 # n'encode pas l'application opérateur -> tâche
-end
+
 # Idée pendant le parcours de l'arbre: pour éviter une explosion en complexité trop problématique, on ne mémorise que les tâches adressées, opérateurs et machines assignés indépendemment de tout mappage entre les tâches et ces derniers. C'est possible car une tâche adressée n'est plus à traiter/décider, et les machines et les opérateurs n'ont pas le don d'ubiquité. Donc pour aller plus loin en profondeur, seules ces informations sont suffisantes pour créer les nœuds.
 
 # Pour créer les assignations des machines et des opérateurs à leurs tâches une fois ces ensembles (machines, opérateurs, tâches) détérminés [sous réserve qu'ils soient entièrement compatibles], il faudra créer un petit solveur en aval une fois l'exploration des possibles effectuée.
@@ -136,25 +129,43 @@ dist = Bernoulli(.1);
 C = rand(dist, 10, 10);
 C = sprand(Bool, 10, 10, .1);
 
-function local_decision(duration_task::Vector{Int64},
-    C::Array{Bool, 3}, # alias de compat_machine_operator_per_task
-    α::Int, β::Int, nb_machines::Int, nb_tasks::Int, nb_jobs::Int, nb_operators::Int,
-    jobs_task_sequences::Dict{Int64, Queue{Int64}}, # à ce stade, n'ont pas été dépilées !
-    # ajouter un dict de vecteurs pour avoir l'accès rapide aux tâches  qui arrivent ensuite
-    jobs_weights::Vector{Int64},
-    jobs_release_date::Vector{Int64},
-    jobs_due_date::Vector{Int64},
-    last_task_of_jobs::Vector{Int64},
-    job_of_task::Vector{Int64},
-    todo_tasks::Set{Int64}, ## paramètres spécifiques au flow dans la décision (à t fixé)
-    Dm::Vector{Bool}, # alias de .~ busy_machines
-    Do::Vector{Bool}) # alias de .~ busy_operators
 
-    C_sparse = Dict{Int64, SparseMatrixCSC{Bool, Int64}};
+using AutoHashEquals;
+@auto_hash_equals struct State
+    nb_todo    ::Int64         # le nombre de tâches à adresser
+    todo       ::Vector{Bool}   # les tâches à adresser 
+    score      ::Number   # score de la configuration (Int ou Float indéfini)
+    av_m       ::UInt128  # n'encode pas l'application m2t:  machine ↦ tâche
+    av_o       ::UInt128  # n'encode pas l'application m2o:opérateur ↦ tâche
+    timestep   ::Int64    # temps
+end
+
+
+function local_decision(duration_task::Vector{Int64},
+    compat_m_o           ::Array{Bool, 3}, # alias pour compat_machine_operator_per_task
+    α                    ::Number,
+    β                    ::Number,
+    nb_machines          ::Int,
+    nb_tasks             ::Int,
+    nb_jobs              ::Int,
+    nb_operators         ::Int,
+    jobs_task_sequences  ::Dict{Int64, Queue{Int64}}, # à ce stade, n'ont pas été dépilées !
+    # ajouter un dict de vecteurs pour avoir l'accès rapide aux tâches  qui arrivent ensuite
+    jobs_weights         ::Vector{Int64},
+    jobs_release_date    ::Vector{Int64},
+    jobs_due_date        ::Vector{Int64},
+    last_task_of_jobs    ::Vector{Int64},
+    job_of_task          ::Vector{Int64},
+    todo_tasks           ::Set{Int64},    # paramètres spécifiques au flow dans la décision (à t fixé)
+    av_m                 ::UInt128,       # alias de .~ busy_machines  (machines disponibles)
+    av_o                 ::UInt128)       # alias de .~ busy_operators (opérateurs disponibles)
+
+    # SPARSIFIER LES COMPATIBILITÉS POUR CHAQUE TÂCHE
+    compat_sparse = Dict{Int64, SparseMatrixCSC{Bool, Int64}}; # créer un dictionnaire ayant pour clé les tâches, et en valeurs les tranches sparsifiées de leur compatibilité (pour itérer dessus ensuite)
     adressable_tasks = Set{Int64}();
-    for τ in  todo_tasks
-        C_sparse[τ] = sparse(view(C,τ,:,:));
-        if @views dot(Dm, C_sparse[τ], Do) ≥ 1
+    for τ in  todo_tasks                  # remplir le dictionnaire en itérant sur les tâches
+        compat_sparse[τ] = sparse(view(compat_m_o, τ,:,:)); # ajouter la tranche
+        if @views dot(av_m, compat_sparse[τ], av_o) ≥ 1
             push!(adressable_tasks, τ);
         end
     end
@@ -162,48 +173,51 @@ function local_decision(duration_task::Vector{Int64},
     nb_adr_tasks         = length(adressable_tasks);
     adressable_tasks_vec = Vector(adressable_tasks);
 
-    collision_machines   = zeros(Bool, nb_adr_tasks, nb_adr_tasks);
-    collision_operators  = zeros(Bool, nb_adr_tasks, nb_adr_tasks);
 
-    for s in Iterators.product(1:nb_adr_tasks, 1:nb_adr_tasks)
-        collisions_machines[s[1],s[2]]  = @views any(Dm' * (C[s[1],:,:] .| C[s[2],:,:]));
-        collisions_operators[s[1],s[2]] = @views any((C[s[1],:,:] .| C[s[2],:,:]) * Do);
+    # CRÉER LES INDICATEURS DE COLLISIONS
+    collision_machines   = zeros(Bool, nb_adr_tasks, nb_adr_tasks);
+    # collision_machines[i,j] = true ssi les tâches i et j partagent au moins une machine disponible étant capable de les réaliser
+    collision_operators  = zeros(Bool, nb_adr_tasks, nb_adr_tasks);
+    # collision_operators[i,j] = true ssi les tâches i et j partagent au moins un opérateur disponible étant capable de les réaliser
+
+    for (t1,t2) in Iterators.product(1:nb_adr_tasks, 1:nb_adr_tasks)
+        collisions_machines[t1,t2]  = @views any(av_m' * (compat_m_o[s[1],:,:] .| C[s[2],:,:]));
+        collisions_operators[t1,t2] = @views any((compat_m_o[s[1],:,:] .| compat_m_o[s[2],:,:]) * av_o);
     end
 
+
+    # CRÉER LE SETUP POUR LE PARCOURS DE L'ARBRE
     encoding_kernel = 2 .^ (0:nb_adr_tasks-1);
 
     q = Queue{State}(); # vérifier au moment d'insérer ou remplacer par un ensemble éventuellement (pas besoin de multiplicité dans la file)
 
+    current_state = State(nb_adr_tasks,0.0)
     enqueue!(q, [current_state])
 
+    # PARCOURS
     while ~isempty(q)
         instance       = dequeue!(q);
         # Vector{Int64} transformés en Vector{Bool}
-        disp_m         = Vector{Bool}(digits(instance.Dm,       base=2, pad=nb_machines));
-        disp_o         = Vector{Bool}(digits(instance.Dm,       base=2, pad=nb_operators));
-        adressable     = Vector{Bool}(digits(instance.assigned, base=2, pad=nb_adr_tasks));
-        score          = instance.score;
+        av_m           = Vector{Bool}(digits(instance.av_m,       base=2, pad=nb_machines));
+        av_o           = Vector{Bool}(digits(instance.av_m,       base=2, pad=nb_operators));
+        adressable     = Vector{Bool}(digits(instance.adressable, base=2, pad=nb_adr_tasks));
+        coonf_score    = instance.conf_score;
         
         for τ=1:nb_adr_tasks # pour toute tâche encore adressable: prendre les bits positifs dans l'ordre du plus 
-            if @views dot(disp_m, C_sparse[τ], disp_o) ≥ 1 # si adressable avec au moins une des ressources restantes
-                c_poss = @views disp_m * ones(Bool, nb_operators)' .& C_sparse[τ] .& ones(Bool, nb_machines) * disp_o';
+            if @views dot(av_m, compat_sparse[τ], av_o) ≥ 1 # si adressable avec au moins une des ressources restantes
+                c_poss = @views av_m * ones(Bool, nb_operators)' .& compat_sparse[τ] .& ones(Bool, nb_machines) * av_o';
                 new_adressable = adressable - encoding_kernel[τ];
                 for (m,o,~) in zip(findnz(c_poss)) # parcours sur la tranche de compatibilité là où sont les 1
-                    
-                    
                 end 
             end
         end
     end
-
-
-    
 end
 
 
 
-
-function distribution(duration_task::Vector{Int64},
+# fonction principale du programme qui calcule la stratégie à adopter
+function main_strategy(duration_task::Vector{Int64},
     compat_machine_operator_per_task::Array{Bool, 3},
     α::Int, β::Int, nb_machines::Int, nb_tasks::Int, nb_jobs::Int, nb_operators::Int,
     jobs_task_sequences::Dict{Int64, Queue{Int64}},
@@ -238,10 +252,12 @@ function distribution(duration_task::Vector{Int64},
     machine_choice_of_task  = zeros(Int64, nb_tasks)
 
 
-    t = 1; # time
-    io = open("C:/Users/matth/Documents/GitHub/kiro-challenge-2022/log_algo.txt", "w");
+    t = 1; # Initialisation du temps
+    log_file = open("C:/Users/matth/Documents/GitHub/kiro-challenge-2022/log_algo.txt", "w"); # ouverture du fichier Log
+    
+    # BOUCLE PRINCIPALE DE DÉCISION
     while (length(done_tasks) < nb_tasks) & (t < 100)
-        write(io, "=== Time $t ===\n");
+        write(log_file, "=== Time $t ===\n");
         for τ in running_tasks # mettre à jour les statuts des tâches déjà démarrées: ont-elles terminé ?
             if t - start_time_of_task[τ] >= duration_task[τ] + 1
                 delete!(running_tasks, τ);
@@ -251,12 +267,12 @@ function distribution(duration_task::Vector{Int64},
 
                 busy_machines[machine_choice_of_task[τ]]   = false;
                 busy_operators[operator_choice_of_task[τ]] = false;
-                write(io, "Finishing task $τ\n");
+                write(log_file, "Finishing task $τ\n");
                 if τ in last_task_of_jobs
                     γ = job_of_task[τ];
                     jobs_completion_time[γ] = t - 1;
                     push!(done_jobs, γ);
-                    write(io, "Completing job $γ\n");
+                    write(log_file, "Completing job $γ\n");
                 end
             end
         end
@@ -266,7 +282,7 @@ function distribution(duration_task::Vector{Int64},
             if ~running_jobs[γ] && ~isempty(jobs_task_sequences[γ]) # dernière tâche du job finie ou bien job pas encore commencé et il reste des tâches: on les ajoute à la liste des todo
                 τ = dequeue!(jobs_task_sequences[γ]);
                 push!(todo_tasks, τ);  # on passe à la tâche suivante
-                write(io, "Adding task $τ to the queue\n");
+                write(log_file, "Adding task $τ to the queue\n");
             end
         end
 
@@ -283,12 +299,12 @@ function distribution(duration_task::Vector{Int64},
         priority = reverse(sortperm(score_of_task[todo_tasks_vec])); # trié dans l'ordre croissant sans le rev
         # mxval, mxindx = findmax(collect(score_of_task));
         tasks_to_assign = todo_tasks_vec[priority];
-        write(io, "Tasks to assign: $tasks_to_assign\n\n");
+        write(log_file, "Tasks to assign: $tasks_to_assign\n\n");
 
         for τ in tasks_to_assign # for i=1:size(tasks_to_assign)[1]
-            # en itérant sur les tâches les plus importantes par ordre décroissantà mesure que l'on parcourt les index (numéro i, i ∈ 1,...)
+            # en itérant sur les tâches les plus importantes par ordre décroissant à mesure que l'on parcourt les index (numéro i, i ∈ 1,...)
 
-            busy_resources .= Matrix{Bool}(ones(Bool, nb_machines)*busy_operators' .| busy_machines*ones(Bool, nb_operators)')[:,:]; # calcul opérateur occupé OU LOGIQUE machine occupée
+            busy_resources .= Matrix{Bool}(ones(Bool, nb_machines)*busy_operators' .| busy_machines*ones(Bool, nb_operators)')[:,:]; # calcul [opérateur occupé] OU LOGIQUE [machine occupée]
             compatible_resources = compat_machine_operator_per_task[τ,:,:];
             possible_resources   = compatible_resources .& .~busy_resources; # matrice (machine, opérateur) ressource dispo si et seulement si elle est compatible (avec la tâche) et disponible
 
@@ -307,24 +323,21 @@ function distribution(duration_task::Vector{Int64},
                 start_time_of_task[τ]      = t;
                 delete!(todo_tasks, τ);
 
-                write(io, "= Commencing task $τ of Job $(job_of_task[τ])\n");
+                write(log_file, "= Commencing task $τ of Job $(job_of_task[τ])\n");
                 push!(running_tasks, τ);
                 running_jobs[job_of_task[τ]] = true;
 
                 busy_operators[choice_operator]  = true;
                 busy_machines[choice_machine]    = true;
-                write(io, "operator $(choice_operator) on machine $(choice_machine)\n");
+                write(log_file, "operator $(choice_operator) on machine $(choice_machine)\n");
             else
-                write(io, "= Postponed task $τ of Job $(job_of_task[τ]) for lack of resource\n");
+                write(log_file, "= Postponed task $τ of Job $(job_of_task[τ]) for lack of resource\n");
             end
         end 
         t += 1;
-        write(io, "\n\n");
+        write(log_file, "\n\n");
     end
-    close(io);
-
-    
-    
+    close(log_file);
 
     sol_cost = solution_cost(nb_jobs, jobs_weights, start_time_of_task, duration_task, jobs_due_date, jobs_completion_time, last_task_of_jobs, α, β);
     return sol_cost, start_time_of_task, busy_resources, jobs_release_date, compat_machine_operator_per_task
@@ -344,7 +357,7 @@ job_of_task = import_init("C:/Users/matth/Documents/GitHub/kiro-challenge-2022/i
 
 
 
-@time sol_cost, start_time_of_task, busy_resources, jobs_release_date, compat_machine_operator_per_task = distribution(duration_task,
+@time sol_cost, start_time_of_task, busy_resources, jobs_release_date, compat_machine_operator_per_task = main_strategy(duration_task,
 compat_machine_operator_per_task,
 α, β, nb_machines, nb_tasks, nb_jobs, nb_operators,
 jobs_task_sequences,
@@ -355,8 +368,14 @@ last_task_of_jobs,
 job_of_task);
 
 
-# survey les collisions de couples opérateurs_machines et faire un histogramme pour ensuite optimiser le code
 
+
+
+
+### BROUILLON
+
+
+# survey les collisions de couples opérateurs_machines et faire un histogramme pour ensuite optimiser le code
 intersection = Float64[];
 for i in 1:nb_tasks
     for j in i+1:nb_tasks
